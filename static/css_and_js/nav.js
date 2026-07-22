0c4aef97919f7b924265442137df609b22a2e093
aadarshkumar-dev <dev.aadarshkumar@gmail.com>
2026-07-22 13:37:26 +0530

    dfd

--- static/css_and_js/nav.js
+++ static/css_and_js/nav.js
@@ -4,3 +4,24 @@ const sidebar = document.getElementById('sidebar');
 toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.getElementById("toggleBtn");
+    const closeBtn = document.getElementById("closeBtn");
+    const sidebar = document.getElementById("sidebar");
+
+    // Open sidebar
+    if (toggleBtn && sidebar) {
+        toggleBtn.addEventListener("click", () => {
+            sidebar.classList.add("active");
+        });
+    }
+
+    // Close sidebar via Cross Button
+    if (closeBtn && sidebar) {
+        closeBtn.addEventListener("click", () => {
+            sidebar.classList.remove("active");
+        });
+    }
+
+});
